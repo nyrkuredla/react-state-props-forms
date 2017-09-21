90eef0fd321f996b4b0c0c5f49038f813b9ab77d
Ryan Kelley <rtkelley@gmail.com>
2017-09-21 13:49:30 -0400

    created all components and rearranged files, App is wired correctly for styles

---
+++ react-state-props-forms/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import '../styles/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App jumbotron">
+        <h1>HELLO!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
